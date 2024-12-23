353e13f1b197261ff17221bf7b2bd243cd37ef06
DLRoux2o20 <153112792+DLRoux2o20@users.noreply.github.com>
2024-12-23 23:22:35 +0200

    Make canvas size change depending on user input. Make divs change to random color when hovering over with mouse.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let container = document.getElementById("container");
+let canvasSizeButton = document.getElementById("canvasSizeButton");
+let rgbValue1 = 50;
+let rgbValue2 = 50;
+let rgbValue3 = 150;
 
 let gridPiece1 = document.createElement("div");
 let gridPiece2 = document.createElement("div");
@@ -17,22 +21,22 @@ let gridPiece14 = document.createElement("div");
 let gridPiece15 = document.createElement("div");
 let gridPiece16 = document.createElement("div");
 
-gridPiece1.className = "grid-piece";
-gridPiece2.className = "grid-piece";
-gridPiece3.className = "grid-piece";
-gridPiece4.className = "grid-piece";
-gridPiece5.className = "grid-piece";
-gridPiece6.className = "grid-piece";
-gridPiece7.className = "grid-piece";
-gridPiece8.className = "grid-piece";
-gridPiece9.className = "grid-piece";
-gridPiece10.className = "grid-piece";
-gridPiece11.className = "grid-piece";
-gridPiece12.className = "grid-piece";
-gridPiece13.className = "grid-piece";
-gridPiece14.className = "grid-piece";
-gridPiece15.className = "grid-piece";
-gridPiece16.className = "grid-piece";
+gridPiece1.className = "grid-piece hover";
+gridPiece2.className = "grid-piece hover";
+gridPiece3.className = "grid-piece hover";
+gridPiece4.className = "grid-piece hover";
+gridPiece5.className = "grid-piece hover";
+gridPiece6.className = "grid-piece hover";
+gridPiece7.className = "grid-piece hover";
+gridPiece8.className = "grid-piece hover";
+gridPiece9.className = "grid-piece hover";
+gridPiece10.className = "grid-piece hover";
+gridPiece11.className = "grid-piece hover";
+gridPiece12.className = "grid-piece hover";
+gridPiece13.className = "grid-piece hover";
+gridPiece14.className = "grid-piece hover";
+gridPiece15.className = "grid-piece hover";
+gridPiece16.className = "grid-piece hover";
 
 container.appendChild(gridPiece1);
 container.appendChild(gridPiece2);
@@ -50,3 +54,31 @@ container.appendChild(gridPiece13);
 container.appendChild(gridPiece14);
 container.appendChild(gridPiece15);
 container.appendChild(gridPiece16);
+
+function randomValues() {
+    rgbValue1 = Math.floor(Math.random() * 255);
+    rgbValue2 = Math.floor(Math.random() * 255);
+    rgbValue3 = Math.floor(Math.random() * 255);
+}
+
+canvasSizeButton.addEventListener("click", function() {
+    let size = prompt("Choose a size between 1-100");
+    let canvasSize = size * size;
+    
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (let i = 0; i < canvasSize; i++) {
+        let addedGridPiece = document.createElement("div");
+        container.appendChild(addedGridPiece);
+        addedGridPiece.className = "hover";
+        addedGridPiece.style.cssText = `width: calc(100% / ${size}); height: calc(100% / ${size}); display: inline-block;`;
+        addedGridPiece.addEventListener("mouseover", function() {
+            console.log("yes1");
+            randomValues();
+            addedGridPiece.style.cssText = `width: calc(100% / ${size}); height: calc(100% / ${size}); display: inline-block; background-color: rgb(${rgbValue1}, ${rgbValue2}, ${rgbValue3});`;
+            console.log("yes2");
+        })
+    }
+})
